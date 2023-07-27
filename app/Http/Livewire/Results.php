9b56e407b408da78f9283c27021064d2a582c067
Gaelcarmona <gaelcarmona.dev@gmail.com>
2023-07-27 16:52:41 +0200

    refacto2

--- app/Http/Livewire/Results.php
+++ app/Http/Livewire/Results.php
@@ -12,6 +12,7 @@ class Results extends Component
     public $results = [];
     public ?array $resultMessages = [];
     protected $listeners = ['searchSubmitted' => 'showResults'];
+    
 
     public function showResults($start, $end)
     {
@@ -37,32 +38,8 @@ class Results extends Component
             return;
         }
 
-        $depth2 = $this->depthEntries(2, $start, $arrival, $dateDebut);
-        if ($depth2) {
-            return;
-        }
-
-        $depth3 = $this->depthEntries(3, $start, $arrival, $dateDebut);
-        if ($depth3) {
-            return;
-        }
-        $depth4 = $this->depthEntries(4, $start, $arrival, $dateDebut);
-        if ($depth4) {
-            return;
-        }else {
-            $this->resultMessages[] = 'On a pas trouvé :/';
-            return;
-        }
-    }
-
-    public function depthEntries($depth, $start, $arrival, $dateDebut)
-    {
-        $resultMessages = [];
         $greatParents = $arrival->availableParentEntries;
         $isTrue = false;
-
-        $count3levels = 0;
-        $countPossibilities = 0;
         $greatParent = null;
 
         foreach ($greatParents as $greatParent) {
@@ -74,94 +51,70 @@ class Results extends Component
 
             $parents = $start->availableChildEntries;
 
-            switch ($depth) {
+            $parents = $parents->whereIn('child_entry_id', $potentialsParents);
 
-                case ('2'):
-                    $parents = $parents->whereIn('child_entry_id', $potentialsParents);
-
-                    if ($parents->isNotEmpty()) {
-                        foreach ($parents as $parent) {
-                            $parent = $parent->childEntry;
-                            $resultMessages[] = $start->title . '->' . $parent->title . '->' . $greatParent->title . '->' . $arrival->title . ' ' . $dateDebut->diff(Carbon::now())->format('%h heures %i minutes %s secondes');
-                            $countPossibilities++;
-                            $isTrue = true;
-                            $this->resultMessages = array_merge($this->resultMessages, $resultMessages);
+            if ($parents->isNotEmpty()) {
+                foreach ($parents as $parent) {
+                    $parent = $parent->childEntry;
+                    $this->resultMessages[] = $start->title . '->' . $parent->title . '->' . $greatParent->title . '->' . $arrival->title . ' ' . $dateDebut->diff(Carbon::now())->format('%h heures %i minutes %s secondes');
+                }
+            }
+            $parents = $start->availableChildEntries;
+            if (empty($this->resultMessages)) {
+                foreach ($parents as $parent) {
+                    $parent = $parent->childEntry;
+    
+                    $inBetweenParents = AvailableEntry::query()
+                        ->where('parent_entry_id', $parent->id)
+                        ->whereIn('child_entry_id', $potentialsParents)
+                        ->with('childEntry')
+                        ->get();
+    
+                    if ($inBetweenParents->isNotEmpty()) {
+                        foreach ($inBetweenParents as $inBetweenParent) {
+                            $inBetweenParent = $inBetweenParent->childEntry;
+                            $this->resultMessages[] = $start->title . '->' . $parent->title . '->' . $inBetweenParent->title . '->' . $greatParent->title . '->' . $arrival->title . ' ' . $dateDebut->diff(Carbon::now())->format('%h heures %i minutes %s secondes');
                         }
                     }
-
-                    break;
-                case ('3'):
-
-                    foreach ($parents as $parent) {
-                        $parent = $parent->childEntry;
-
-                        $inBetweenParents = AvailableEntry::query()
-                            ->where('parent_entry_id', $parent->id)
-                            ->whereIn('child_entry_id', $potentialsParents)
+                }
+            }
+            if (empty($this->resultMessages)) {
+                $potentialsInBetween = Entry::query()->whereIn('id', $potentialsParents)->with('availableParentEntries.parentEntry')->get();
+    
+                foreach ($potentialsInBetween as $potentialInBetween) {
+    
+                    $potentialsChilds = $potentialInBetween->availableParentEntries->pluck('parent_entry_id')->toArray();
+                    $potentialsChilds = array_unique($potentialsChilds);
+    
+                    $potentialsInBetween2 = Entry::query()->whereIn('id', $potentialsChilds)->get();
+    
+                    foreach ($potentialsInBetween2 as $potentialInBetween2) {
+                        $linked = AvailableEntry::query()
+                            ->where('parent_entry_id', $potentialInBetween2->id)
+                            ->where('child_entry_id', $potentialInBetween->id)
                             ->with('childEntry')
-                            ->get();
-
-                        if ($inBetweenParents->isNotEmpty()) {
-                            foreach ($inBetweenParents as $inBetweenParent) {
-                                $inBetweenParent = $inBetweenParent->childEntry;
-                                $resultMessages[] = $start->title . '->' . $parent->title . '->' . $inBetweenParent->title . '->' . $greatParent->title . '->' . $arrival->title . ' ' . $dateDebut->diff(Carbon::now())->format('%h heures %i minutes %s secondes');
-                                $count3levels++;
-                                $isTrue = true;
-                                $this->resultMessages = array_merge($this->resultMessages, $resultMessages);
-                            }
-                        }
-                        $countPossibilities = $count3levels;
-                    }
-
-                    break;
-                case ('4'):
-
-                    $potentialsInBetween = Entry::query()->whereIn('id', $potentialsParents)->with('availableParentEntries.parentEntry')->get();
-
-                    foreach ($potentialsInBetween as $potentialInBetween) {
-
-                        $potentialsChilds = $potentialInBetween->availableParentEntries->pluck('parent_entry_id')->toArray();
-                        $potentialsChilds = array_unique($potentialsChilds);
-
-                        $potentialsInBetween2 = Entry::query()->whereIn('id', $potentialsChilds)->get();
-
-                        foreach ($potentialsInBetween2 as $potentialInBetween2) {
-                            $linked = AvailableEntry::query()
-                                ->where('parent_entry_id', $potentialInBetween2->id)
-                                ->where('child_entry_id', $potentialInBetween->id)
-                                ->with('childEntry')
-                                ->with('parentEntry')
-                                ->first();
-                            if ($linked !== null) {
-                                $first = $linked->parentEntry;
-                                $second = $linked->childEntry;
-
-                                if ($parents->isNotEmpty()) {
-                                    foreach ($parents as $parent) {
-                                        $parent = $parent->childEntry;
-                                        foreach ($parent->availableChildEntries as $availableChildEntry) {
-                                            if ($availableChildEntry->child_entry_id == $first->id) {
-                                                $resultMessages[] = $start->title . '->' . $parent->title . '->' . $first->title . '->' . $second->title . '->' . $greatParent->title . '->' . $arrival->title . ' ' . $dateDebut->diff(Carbon::now())->format('%h heures %i minutes %s secondes');
-                                                $count3levels++;
-                                                $countPossibilities = $count3levels;
-                                                $isTrue = true;
-                                                $this->resultMessages = array_merge($this->resultMessages, $resultMessages);
-                                            }
+                            ->with('parentEntry')
+                            ->first();
+                        if ($linked !== null) {
+                            $first = $linked->parentEntry;
+                            $second = $linked->childEntry;
+    
+                            if ($parents->isNotEmpty()) {
+                                foreach ($parents as $parent) {
+                                    $parent = $parent->childEntry;
+                                    foreach ($parent->availableChildEntries as $availableChildEntry) {
+                                        if ($availableChildEntry->child_entry_id == $first->id) {
+                                            $this->resultMessages[] = $start->title . '->' . $parent->title . '->' . $first->title . '->' . $second->title . '->' . $greatParent->title . '->' . $arrival->title . ' ' . $dateDebut->diff(Carbon::now())->format('%h heures %i minutes %s secondes');
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                    break;
-
-                default:
-                    $msg = 'Something went wrong.';
+                }
             }
         }
-        return $isTrue;
     }
-
 
     public function render()
     {
