18190360b72ca4b3c55fcb1d4a964c69c8f48116
Gaelcarmona <gaelcarmona.dev@gmail.com>
2023-08-27 05:49:53 +0200

    front v2 + feat shuffle

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -22,11 +22,11 @@ class Search extends Component
         if ($this->start != '') {
             $this->startSearchResults =
                 Entry::query()
-                    ->where('paths', '!=', null)
-                    ->where('title', 'like', '%' . $this->start . '%')
-                    ->limit(10)
-                    ->pluck('title')
-                    ->toArray();
+                ->where('paths', '!=', null)
+                ->where('title', 'like', '%' . $this->start . '%')
+                ->limit(10)
+                ->pluck('title')
+                ->toArray();
         } else {
             $this->startSearchResults = [];
         }
@@ -37,13 +37,51 @@ class Search extends Component
         if ($this->end != '') {
             $this->endSearchResults =
                 Entry::query()
-                    ->where('paths', '!=', null)
-                    ->where('title', 'like', '%' . $this->end . '%')
-                    ->limit(10)
-                    ->pluck('title')
-                    ->toArray();
+                ->where('paths', '!=', null)
+                ->where('title', 'like', '%' . $this->end . '%')
+                ->limit(10)
+                ->pluck('title')
+                ->toArray();
         } else {
             $this->endSearchResults = [];
+        }
+    }
+
+    public function shuffleStart()
+    {
+        $correctEntry = false;
+
+        while (!$correctEntry) {
+            $randomId = rand(1, 8000000);
+            $randomStart =
+                Entry::query()
+                ->where('id', '=', $randomId)
+                ->first();
+                if ($randomStart != null) {
+                    if ($randomStart->paths != null) {
+                        $correctEntry = true;
+                        $this->start = $randomStart->title;
+                    }
+                }
+        }
+    }
+
+    public function shuffleEnd()
+    {
+        $correctEntry = false;
+
+        while (!$correctEntry) {
+            $randomId = rand(1, 8000000);
+            $randomEnd =
+                Entry::query()
+                ->where('id', '=', $randomId)
+                ->first();
+                if ($randomEnd != null) {
+                    if ($randomEnd->paths != null) {
+                        $correctEntry = true;
+                        $this->end = $randomEnd->title;
+                    }
+                }
         }
     }
 
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,50 +1,84 @@
 <div>
     @unless ($formSubmitted)
-
-        <form wire:submit.prevent="submit" class="space-y-8 divide-y divide-gray-200">
-            <div class="text-center">
-
-                <div>
-                    <label>
-                        <input type="text" placeholder="Page de départ" list="startOptions"
-                               wire:model.debounce.500ms="start"
-                               class="rounded" x-ref="startInput">
-                        <datalist id="startOptions">
-                            @foreach ($startSearchResults as $result)
-                                <option wire:key="{{ $result }}" data-value="{{ $result }}" value="{{ $result }}">
-                                </option>
-                            @endforeach
-                        </datalist>
-                    </label>
-                    <label>
-                        <input type="text" placeholder="Page d'arrivée" list="endOptions"
-                               wire:model.debounce.500ms="end"
-                               class="rounded" x-ref="endInput">
-                        <datalist id="endOptions">
-                            @foreach ($endSearchResults as $result)
-                                <option wire:key="{{ $result }}" data-value="{{ $result }}" value="{{ $result }}">
-                                </option>
-                            @endforeach
-                        </datalist>
-
-                    </label>
-
+        <div class="flex items-center justify-center h-screen">
+            <div class="h-100 w-100 md:h-172 md:w-172 bg-gray-300 flex items-center justify-center rounded p-3">
+                <div class="md:w-1/2 md:pl-4 md:pt-0 md:pt-4 md:order-1">
+                    <img src="{{ asset('images/six_degrees_of_separation.svg.png') }}" alt="six_degrees_of_separation">
                 </div>
-                <div>
-                    <label>
-                        <input type="checkbox" x-ref="allShortestPathsInput"
-                               wire:model="allShortestPaths"/>
-                        Afficher tous les chemins les plus rapides
-                        (recherche plus longue).
-                    </label>
-                </div>
-                <button type="submit" class="text-white font-bold  px-4 rounded" style="background-color: #03a696">
-                    Rechercher
-                </button>
+                <div class="md:w-1/2 md:pr-4 md:pb-0 md:pb-4 md:order-2">
+                    <p>Bienvenue sur notre site dédié à l'exploration fascinante des liens qui unissent le vaste univers de
+                        la connaissance. Inspiré par la théorie des <a style="color: #03a696" class="navbar-brand text-white"
+                            href="https://fr.wikipedia.org/wiki/Six_degrés_de_séparation" target="_blank">six degrés de
+                            séparation</a> de <a style="color: #03a696" class="navbar-brand text-white"
+                            href="https://fr.wikipedia.org/wiki/Frigyes_Karinthy" target="_blank">Frigyes Karinthy</a>,
+                        notre
+                        plateforme vous invite à découvrir la magie qui se cache derrière chaque clic. Parcourez les
+                        méandres des pages Wikipedia à travers des parcours de graphe ingénieux, et laissez-vous émerveiller
+                        par les connexions qui relient des personnages, des lieux, des concepts et bien plus encore.</p>
+                    <br>
+                    <p>Notre objectif est simple : vous offrir un moyen captivant et ludique de mettre en lumière les
+                        chemins inattendus qui tissent la toile de la connaissance. Plongez dans le défi des wikispeedruns,
+                        explorez les méta-recherches et prouvez par vous-même la théorie des six degrés de séparation en
+                        découvrant comment chaque coin de la vaste encyclopédie en ligne est interconnecté.</p>
+                    <br>
+                    <p>Rejoignez-nous dans cette aventure où chaque clic vous rapproche davantage de la découverte, où
+                        chaque lien vous guide vers de nouvelles perspectives. Bienvenue sur notre plateforme d'exploration
+                        et de jeu, où la curiosité est votre guide et où la découverte est votre récompense.</p>
+                    <br>
+                    <form wire:submit.prevent="submit" class="space-y-8 divide-y divide-gray-200">
+                        <div class="text-center">
+                            <div class="flex flex-col space-y-4">
+                                <div class="flex items-center mx-3">
+                                    <label class="flex-grow h-full w-40">
+                                        <input type="text" placeholder="Page de départ" list="startOptions"
+                                            wire:model.debounce.500ms="start" class="rounded w-full" x-ref="startInput">
+                                        <datalist id="startOptions">
+                                            @foreach ($startSearchResults as $result)
+                                                <option wire:key="{{ $result }}" data-value="{{ $result }}"
+                                                    value="{{ $result }}">
+                                                </option>
+                                            @endforeach
+                                        </datalist>
+                                    </label>
+                                    <button type="button" class="border-solid border-2 border-black h-full"
+                                        wire:click="shuffleStart">
+                                        <img src="{{ asset('images/shuffle-svgrepo-com.svg') }}" alt="shuffle">
+                                    </button>
+                                </div>
+                                <div class="flex items-center mx-3">
+                                    <label class="flex-grow h-full w-40">
+                                        <input type="text" placeholder="Page d'arrivée" list="endOptions"
+                                            wire:model.debounce.500ms="end" class="rounded w-full" x-ref="endInput">
+                                        <datalist id="endOptions">
+                                            @foreach ($endSearchResults as $result)
+                                                <option wire:key="{{ $result }}" data-value="{{ $result }}"
+                                                    value="{{ $result }}">
+                                                </option>
+                                            @endforeach
+                                        </datalist>
+                                    </label>
+                                    <button type="button" class="border-solid border-2 border-black h-full"
+                                        wire:click="shuffleEnd">
+                                        <img src="{{ asset('images/shuffle-svgrepo-com.svg') }}" alt="shuffle">
+                                    </button>
+                                </div>
+                            </div>
+                            <div>
+                                <label>
+                                    <input type="checkbox" x-ref="allShortestPathsInput" wire:model="allShortestPaths" />
+                                    Afficher tous les chemins les plus rapides
+                                    (recherche plus longue).
+                                </label>
+                            </div>
+                            <button type="submit" class="text-white font-bold  px-4 rounded"
+                                style="background-color: #03a696">
+                                Rechercher
+                            </button>
 
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+        </div>
     @endunless
 </div>
-
-
