eedc8ad6e10d0d2caab5259abcf78f206ff9fa0b
GaelGlanum <gael@glanum.com>
2023-09-04 15:42:54 +0200

    refacto input et shuffle front

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -2,7 +2,8 @@
     <div class="md:w-1/2 md:pl-4 md:pt-0 md:pt-4 md:order-1 hidden md:block">
         <img src="{{ asset('images/six_degrees_of_separation.svg.png') }}" alt="six_degrees_of_separation">
     </div>
-<div class="md:w-1/2 md:pr-4 md:pb-0 md:pb-4 md:order-2 mx-3 items-center w-full lg:w-1/2 lg:pr-4 lg:pb-0 lg:pb-4 lg:order-2">
+    <div
+        class="md:w-1/2 md:pr-4 md:pb-0 md:pb-4 md:order-2 mx-3 items-center w-full lg:w-1/2 lg:pr-4 lg:pb-0 lg:pb-4 lg:order-2">
         <div class="hidden lg:block">
             <p>Bienvenue sur notre site dédié à l'exploration fascinante des liens qui unissent le vaste univers de
                 la connaissance. Inspiré par la théorie des <a style="color: #03a696" class="navbar-brand text-white"
@@ -30,68 +31,65 @@
         <form wire:submit.prevent="submit" class="space-y-8 divide-y divide-gray-200">
             <div class="text-center">
                 <div class="flex flex-col space-y-4">
-                    <div class="flex items-center mx-3">
-                        <div class="flex-grow h-full ">
-                            <input type="text"
-                                   class="bg-gray-100 focus:bg-white border-solid border-2 border-black rounded-tl rounded-bl w-full text-xl"
-                                   placeholder="Page de départ" wire:model.debounce.250ms="start">
-                            @if (!empty($startSearchResults))
-                                <div class="relative">
-                                    <div class="absolute top-100 mt-1 w-full border bg-white shadow-xl rounded">
-                                        <ul class="p-3">
-                                            @foreach ($startSearchResults as $result)
-                                                @php
-                                                    $resultWithoutLashes = str_replace("'", '.', $result);
-                                                @endphp
-                                                <li>
-                                                    <p class="p-2 flex block w-full rounded hover:bg-gray-100"
-                                                       wire:click="selectStartResult('{{ $resultWithoutLashes }}')">
-                                                        {{ $result }}
-                                                    </p>
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                </div>
-                            @endif
-                        </div>
+                    <div class="relative mx-3">
+                        <input type="text"
+                               class="bg-gray-100 focus:bg-white border-solid border-2 border-black rounded-tl rounded-bl w-full text-xl pr-10"
+                               placeholder="Page de départ" wire:model.debounce.250ms="start">
                         <button type="button"
-                                class="border-solid border-2 border-black h-full bg-white rounded-tr rounded-br"
+                                class="absolute inset-y-0 right-0 border-solid border-2 border-black h-full bg-white rounded-tr rounded-br"
                                 wire:click="shuffleStart">
                             <img src="{{ asset('images/shuffle-svgrepo-com.svg') }}" alt="shuffle">
                         </button>
                     </div>
-                    <div class="flex items-center mx-3">
-                        <div class="flex-grow h-full w-40">
-                            <input type="text"
-                                   class="bg-gray-100 focus:bg-white border-solid border-2 border-black rounded-tl rounded-bl w-full text-xl"
-                                   placeholder="Page d'arrivée" wire:model.debounce.250ms="end">
-                            @if (!empty($endSearchResults))
-                                <div class="relative">
-                                    <div class="absolute top-100 mt-1 w-full border bg-white shadow-xl rounded">
-                                        <ul class="p-3">
-                                            @foreach ($endSearchResults as $result)
-                                                @php
-                                                    $resultWithoutLashes = str_replace("'", '.', $result);
-                                                @endphp
-                                                <li>
-                                                    <p class="p-2 flex block w-full rounded hover:bg-gray-100"
-                                                       wire:click="selectEndResult('{{ $resultWithoutLashes }}')">
-                                                        {{ $result }}
-                                                    </p>
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </div>
-                                </div>
-                            @endif
+                    @if (!empty($startSearchResults))
+                        <div class="relative mx-3">
+                            <div class="absolute top-full w-full border bg-white shadow-xl rounded mt-1">
+                                <ul class="p-3">
+                                    @foreach ($startSearchResults as $result)
+                                        @php
+                                            $resultWithoutLashes = str_replace("'", '.', $result);
+                                        @endphp
+                                        <li>
+                                            <p class="p-2 flex block w-full rounded hover:bg-gray-100"
+                                               wire:click="selectStartResult('{{ $resultWithoutLashes }}')">
+                                                {{ $result }}
+                                            </p>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
                         </div>
+                    @endif
+
+                    <div class="relative mx-3">
+                        <input type="text"
+                               class="bg-gray-100 focus:bg-white border-solid border-2 border-black rounded-tl rounded-bl w-full text-xl pr-10"
+                               placeholder="Page de départ" wire:model.debounce.250ms="end">
                         <button type="button"
-                                class="border-solid border-2 border-black h-full bg-white rounded-tr rounded-br"
+                                class="absolute inset-y-0 right-0 border-solid border-2 border-black h-full bg-white rounded-tr rounded-br"
                                 wire:click="shuffleEnd">
                             <img src="{{ asset('images/shuffle-svgrepo-com.svg') }}" alt="shuffle">
                         </button>
                     </div>
+                    @if (!empty($endSearchResults))
+                        <div class="relative mx-3">
+                            <div class="absolute top-full w-full border bg-white shadow-xl rounded mt-1">
+                                <ul class="p-3">
+                                    @foreach ($endSearchResults as $result)
+                                        @php
+                                            $resultWithoutLashes = str_replace("'", '.', $result);
+                                        @endphp
+                                        <li>
+                                            <p class="p-2 flex block w-full rounded hover:bg-gray-100"
+                                               wire:click="selectStartResult('{{ $resultWithoutLashes }}')">
+                                                {{ $result }}
+                                            </p>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        </div>
+                    @endif
                 </div>
                 @if (!$formSubmitted)
                     <button type="submit" class="text-white font-bold  px-4 rounded my-3 search-button"
